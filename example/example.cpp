c7a62a1ccbe39b5974c103cd825cfc4d569b1b01
SebastianKirchner <sebastian.t.kirchner@gmail.com>
2016-05-25 14:11:18 +0200

    Added GNU GPL to example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -5,6 +5,20 @@
 //  Created by Sebastian on 18/05/16.
 //  Copyright © 2016 Sebastian Kirchner. All rights reserved.
 //
+//	This file is part of LiSK.
+//
+//	LiSK is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	LiSK is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with LiSK.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <iostream>
 #include <iomanip>
